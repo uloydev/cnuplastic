bce4305b9acea71cb14893fcc43ab6eb4bf1e81a
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-02-26 02:23:42 +0700

    [ADD] livewire, tailwind, alphine

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,6 +13,12 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [ tailwindcss('./tailwind.config.js') ],
+    })
+
+if (mix.inProduction()) {
+    mix.version();
+}
