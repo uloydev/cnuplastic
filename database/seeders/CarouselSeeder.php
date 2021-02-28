5e5a7d13b2eb6e66b1a987d4d09865ae3de56a5d
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-02-28 23:20:36 +0700

    [ADD] carousel dummy record

---
+++ database/seeders/CarouselSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Carousel;
+use Illuminate\Database\Seeder;
+
+class CarouselSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Carousel::factory()->count(10)->create();
+    }
+}
