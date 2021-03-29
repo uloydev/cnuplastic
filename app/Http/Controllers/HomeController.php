9e0caba2f4123bc90693e4a80190b38ecdd12415
uloydev <uloydev@gmail.com>
2021-03-29 18:35:38 +0700

    [ADD] static page and HomeController

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\ProductCategory;
+use App\Models\Merchandise;
+use App\Models\MerchandiseCategory;
+
+class HomeController extends Controller
+{
+    public function index() 
+    {
+        return view('main.index');
+    }
+
+    public function faq() 
+    {
+        return view('main.faq');
+    }
+
+    public function products() 
+    {
+        return view('main.products');
+    }
+
+    public function merchandise() 
+    {
+        return view('main.merchandise');
+    }
+    
+    public function about() 
+    {
+        return view('main.about');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::get('/faq', [HomeController::class, 'faq'])->name('faq');
+Route::get('/products', [HomeController::class, 'products'])->name('products');
+Route::get('/merchandise', [HomeController::class, 'merchandise'])->name('merchandise');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
 
 // jangan diapus briq
 require __DIR__.'/auth.php';
