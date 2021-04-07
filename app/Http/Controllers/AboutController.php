989dac7c5a76c31af54c82f9c3e3969ed31ab318
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-04-08 00:44:37 +0700

    [UPDATE] make about page content is CMS

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\About;
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $about = About::firstOrFail();
+        return view('main.about', get_defined_vars());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Update or create new about info
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store()
+    {
+        About::updateOrCreate(
+            ['id' => 1],
+            ['logo' => request('logo'), 'excerpt' => request('excerpt')]
+        );
+        return redirect()->back()->with('success', 'Berhasil mengganti info');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+    
+}
