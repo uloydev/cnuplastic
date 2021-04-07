cb3b69630c55e0f0880987dbdb4cf82bf206d6ff
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-04-08 00:43:51 +0700

    [ADD] faq logic [UPDATE] integrate faq with view

--- resources/views/faq/header.blade.php
+++ resources/views/faq/header.blade.php
@@ -2,6 +2,9 @@
 <h3>Have any question?</h3>
 <div class="menu1">
     @foreach ($headers as $menu)
-        <a href="{{ $menu['url'] }}" class="text-secondary">{{ $menu['text'] }}</a>
+        <a href="{{ $menu['url'] }}" 
+        class="{{ request()->path() == trim(substr(parse_url($menu['url'], PHP_URL_PATH), 1)) ? 'active' : '' }}">
+            {{ $menu['text'] }}
+        </a>
     @endforeach
 </div>
