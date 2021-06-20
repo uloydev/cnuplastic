3d76736b26a189f75982b61e465d40154713053a
uloydev <uloydev@gmail.com>
2021-06-20 21:49:13 +0700

    [FIX] faqs migration

--- database/migrations/2021_04_07_173648_create_faqs_table.php
+++ database/migrations/2021_04_07_173648_create_faqs_table.php
@@ -16,7 +16,7 @@ class CreateFaqsTable extends Migration
         Schema::create('faq', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->enum('category', config('faq_enum'));
+            $table->enum('category', config('app.faq_enum'));
             $table->text('detail');
             $table->timestamps();
         });
